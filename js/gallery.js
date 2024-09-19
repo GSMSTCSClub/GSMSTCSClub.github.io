7c96b334912bd795d1545621c5ab8defb7836980
legitminh <totallylegitperson.example@gmail.com>
2024-09-19 10:56:33 -0400

    feat (gallery): finished zoom out image when click on feature

--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,33 @@
 function imageCopy(src){
     var newComponent = document.createElement('div');
     var image = document.createElement('img');
-    newComponent.style.position = 'absolute';
-    newComponent.width='100%';
-    newComponent.height='100%';
+    newComponent.style.position = 'fixed';
+    newComponent.style.top='0';
+    newComponent.style.left='0';
+    newComponent.style.right='0';
+    newComponent.style.bottom='0';
+
+    newComponent.style.display='flex';
+    newComponent.style.alignItems='center';
+    newComponent.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    var center = document.createElement('center');
 
     image.src = src;
-
-    image.style.maxWidth = '100%';
+    image.style.maxWidth = '75%';
     image.style.maxHeight = '75%';
-    newComponent.appendChild(image);
+    center.appendChild(image)
+    newComponent.appendChild(center);
 
 
+    newComponent.addEventListener("click", ()=>{
+        newComponent.remove();
+    });
     return newComponent;
 }
 document.addEventListener("DOMContentLoaded", (e) => {
     var images = Array.from(document.querySelectorAll('.gallery-image'));
     images.forEach((element) => {
         element.addEventListener("click" , ()=>{
-            console.log("Hi");
             document.getElementById("main").appendChild(imageCopy(element.src));
         })
     });
