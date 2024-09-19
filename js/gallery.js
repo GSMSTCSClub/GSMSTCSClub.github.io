b85a15395ba8a6fa0b65dca7a7544b16ea682fd5
legitMinh <90986973+legitminh@users.noreply.github.com>
2024-09-18 21:37:14 -0400

    doNotPublish InProgress: gallery update

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,25 @@
 function imageCopy(src){
-    var newComponent = document.createElement('img');
-    newComponent.src = src;
-    newComponent.style.width = '100%';
+    var newComponent = document.createElement('div');
+    var image = document.createElement('img');
+    newComponent.style.position = 'absolute';
+    newComponent.width='100%';
+    newComponent.height='100%';
+
+    image.src = src;
+
+    image.style.maxWidth = '100%';
+    image.style.maxHeight = '75%';
+    newComponent.appendChild(image);
+
+
     return newComponent;
 }
-document.addEventListener("load", (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
     var images = Array.from(document.querySelectorAll('.gallery-image'));
     images.forEach((element) => {
-        console.log(element.src);
         element.addEventListener("click" , ()=>{
             console.log("Hi");
-            // document.getElementById("main").appendChild(imageCopy(element.src));
+            document.getElementById("main").appendChild(imageCopy(element.src));
         })
     });
 })
